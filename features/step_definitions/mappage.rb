ec77081992f67747863ca65f392439a86d6f8b4a
meugenia1 <meps@cin.ufpe.br>
2017-10-09 09:42:39 -0300

    Add files via upload

---
+++ features/step_definitions/mappage.rb
@@ -0,0 +1,11 @@
+Given("I am on the map page") do
+  visit "/map"
+end
+
+When("click on the map") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should navigate on it") do
+  visit "maps.google.com"
+end
