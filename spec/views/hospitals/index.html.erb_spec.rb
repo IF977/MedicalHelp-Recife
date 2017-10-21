76af23a80bbe94cf2583d83747a1ffce4b18caf3
= <mmps@cin.ufpe.br>
2017-10-21 12:46:06 -0200

    corrige testes rspec #30

--- spec/views/hospitals/index.html.erb_spec.rb
+++ spec/views/hospitals/index.html.erb_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "hospitals/index", type: :view do
-  before(:each) do
-    assign(:hospitals, [
-      Hospital.create!(),
-      Hospital.create!()
-    ])
-  end
 
-  it "renders a list of hospitals" do
-    render
-  end
 end
--- spec/views/hospitals/show.html.erb_spec.rb
+++ spec/views/hospitals/show.html.erb_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "hospitals/show", type: :view do
-  before(:each) do
-    @hospital = assign(:hospital, Hospital.create!())
-  end
 
-  it "renders attributes in <p>" do
-    render
-  end
 end
