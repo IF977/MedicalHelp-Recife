7716d0974cf7ba2539a33bdc0fabf01f64444d64
Mateus Ponciano <mmps@cin.ufpe.br>
2017-10-25 19:10:50 +0000

    adiciona ssl #47

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module MHR
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    #Add ssl encryption
+    config.middleware.insert_before  ActionDispatch::Cookies, Rack::SslEnforcer
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
