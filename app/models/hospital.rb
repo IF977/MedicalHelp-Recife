aee716ab97dc4aece1554cbde9f88f0752cbecb2
Mariana Lima <mlsb@cin.ufpe.br>
2017-11-01 19:23:38 +0000

    #52 Adiciona latitudes e longitudes com geocoding

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,2 +1,9 @@
 class Hospital < ApplicationRecord
+    geocoded_by :address, :latitude  => :nu_latitude, :longitude => :nu_longitude
+    after_validation :geocode
+    
+    def address
+        [no_logradouro, nu_endereco, no_complemento, no_bairro].compact.join(', ')
+    end
+    
 end
