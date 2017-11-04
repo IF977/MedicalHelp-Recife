495e6ca8e54e609840631593fa7860ee1feeb863
= <mmps@cin.ufpe.br>
2017-11-04 12:40:32 -0200

    adiciona caminho triste para as rotas #61

--- spec/routing/hospitals_routing_spec.rb
+++ spec/routing/hospitals_routing_spec.rb
@@ -11,4 +11,33 @@ RSpec.describe HospitalsController, type: :routing do
     end
 
   end
+
+  describe "routing to invalid urls" do
+    it "routes to #new" do
+      expect(:get => "/new").not_to be_routable
+    end
+
+    it "routes to #show" do
+      expect(:get => "/1").not_to be_routable
+    end
+
+    it "routes to #edit" do
+      expect(:get => "/1/edit").not_to be_routable
+    end
+  end
+
+  describe "routing to invalid actions" do
+    it "routes to POST method" do
+      expect(:post => "/").not_to be_routable
+    end
+
+    it "routes to PUT method" do
+      expect(:put => "/").not_to be_routable
+    end
+
+    it "routes to DELETE method" do
+      expect(:delete => "/").not_to be_routable
+    end
+
+  end
 end
