46858d8e5a458e068e8a0af848ddb397c8d3df0f
= <mmps@cin.ufpe.br>
2017-11-25 17:49:16 -0200

    adiciona testes rspec models #76

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Hospital, type: :model do
+  it "has none to begin with" do
+    expect(Hospital.count).to eq 0
+  end
+
+  it "has one after adding one" do
+    Hospital.create
+    expect(Hospital.count).to eq 1
+  end
+
+  it "has none after one was created in a previous example" do
+    expect(Hospital.count).to eq 0
+  end
 
 end
