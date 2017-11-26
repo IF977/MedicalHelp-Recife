57276c9c5bcbbdaffbd2ff3dc660ee53e18d87fb
= <mmps@cin.ufpe.br>
2017-11-26 17:21:30 -0200

    corrige testes rspec

--- spec/views/hospitals/showmap.html.erb_spec.rb
+++ spec/views/hospitals/showmap.html.erb_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe "hospitals/showmap", type: :view do
 
   before :each do
-    assign(:hospitals, [Hospital.create(nu_latitude: 1, nu_longitude: 2)])
+    assign(:centrossaude, [Hospital.create(nu_latitude: 1, nu_longitude: 2)])
+    assign(:consultorios, [Hospital.create(nu_latitude: 1, nu_longitude: 2)])
+    assign(:clinicas, [Hospital.create(nu_latitude: 1, nu_longitude: 2)])
+    assign(:hospgerais, [Hospital.create(nu_latitude: 1, nu_longitude: 2)])
+    assign(:hospespecializados, [Hospital.create(nu_latitude: 1, nu_longitude: 2)])
     render
   end
 
