74d5271bb7dd2497211faa0fea9ffb2d614f1077
meugenia1 <meps@cin.ufpe.br>
2017-11-26 21:56:44 -0300

    #82

--- features/step_definitions/pingeneralhospital.rb
+++ features/step_definitions/pingeneralhospital.rb
@@ -3,11 +3,11 @@ Given("I am on the mappage") do
 end
 
 And ("there are general hospitals near me") do
-  expect(page).to have_xpath "//script[contains(., icon.pin_hospitalgeral)]", visible: false
+  expect(page).to have_xpath "//script[contains(., icon.geral_u)]", visible: false
 end
 
 Then("I should see a pin on the map on every general hospital location") do
-  expect(page).to have_xpath "//script[contains(., icon.pin_hospitalgeral)]", visible: false
+  expect(page).to have_xpath "//script[contains(., icon.geral_u)]", visible: false
 end
 
 Given("I am on the mappage") do
@@ -15,9 +15,9 @@ Given("I am on the mappage") do
 end
 
 And ("there are not general hospitals near me") do
-  expect(page).not_to have_xpath "//script[contains(., icon.pin_hospitalgeral)]", visible: false
+  expect(page).not_to have_xpath "//script[contains(., icon.geral_u)]", visible: false
 end
 
 Then("I should not see a general hospital pin on the map") do
-  expect(page).not_to have_xpath "//script[contains(., icon.pin_hospitalgeral)]", visible: false
+  expect(page).not_to have_xpath "//script[contains(., icon.geral_u)]", visible: false
 end
