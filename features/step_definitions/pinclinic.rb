4768d9a457b6ab0ac5a03a42ffc6ccbe600b1c22
meugenia1 <meps@cin.ufpe.br>
2017-11-26 21:55:55 -0300

    #82

--- features/step_definitions/pinclinic.rb
+++ features/step_definitions/pinclinic.rb
@@ -3,11 +3,11 @@ Given("I am on the mappage") do
 end
 
 And ("there are clinics near me") do
-  expect(page).to have_xpath "//script[contains(., icon.pin_consultorio)]", visible: false
+  expect(page).to have_xpath "//script[contains(., icon.consult_u)]", visible: false
 end 
 
 Then("I should see a pin on the map on every clinic location") do
-  expect(page).to have_xpath "//script[contains(., icon.pin_consultorio)]", visible: false
+  expect(page).to have_xpath "//script[contains(., icon.consult_u)]", visible: false
 end
 
 
@@ -16,9 +16,9 @@ Given("I am on the mappage") do
 end
 
 And ("there are not clinics near me") do
-  expect(page).not_to have_xpath "//script[contains(., icon.pin_consultorio)]", visible: false
+  expect(page).not_to have_xpath "//script[contains(., icon.consult_u)]", visible: false
 end
 
 Then("Then I should not see a clinic pin on the map") do
-  expect(page).not_to have_xpath "//script[contains(., icon.pin_consultorio)]", visible: false
+  expect(page).not_to have_xpath "//script[contains(., icon.consult_u)]", visible: false
 end
