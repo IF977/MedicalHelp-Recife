b332c447ac981ff74e1c3a4dcad4baa2af0304fb
= <mmps@cin.ufpe.br>
2017-11-26 23:23:36 -0200

    correcoes

--- features/step_definitions/pinclinic.rb
+++ features/step_definitions/pinclinic.rb
@@ -1,24 +1,15 @@
-Given("I am on the mappage") do
-  visit "/map"
-end
-
 And ("there are clinics near me") do
   expect(page).to have_xpath "//script[contains(., icon.consult_u)]", visible: false
-end 
+end
 
 Then("I should see a pin on the map on every clinic location") do
   expect(page).to have_xpath "//script[contains(., icon.consult_u)]", visible: false
 end
 
-
-Given("I am on the mappage") do
-  visit "/map"
-end
-
 And ("there are not clinics near me") do
-  expect(page).not_to have_xpath "//script[contains(., icon.consult_u)]", visible: false
+  expect(page).not_to have_xpath "//script[contains(., icon.consult_u)]"
 end
 
-Then("Then I should not see a clinic pin on the map") do
-  expect(page).not_to have_xpath "//script[contains(., icon.consult_u)]", visible: false
+Then("I should not see a clinic pin on the map") do
+  expect(page).not_to have_xpath "//script[contains(., icon.consult_u)]"
 end
--- features/step_definitions/pingeneralhospital.rb
+++ features/step_definitions/pingeneralhospital.rb
@@ -1,7 +1,3 @@
-Given("I am on the mappage") do
-  visit "/map"
-end
-
 And ("there are general hospitals near me") do
   expect(page).to have_xpath "//script[contains(., icon.geral_u)]", visible: false
 end
@@ -10,14 +6,10 @@ Then("I should see a pin on the map on every general hospital location") do
   expect(page).to have_xpath "//script[contains(., icon.geral_u)]", visible: false
 end
 
-Given("I am on the mappage") do
-  visit "/map"
-end
-
 And ("there are not general hospitals near me") do
-  expect(page).not_to have_xpath "//script[contains(., icon.geral_u)]", visible: false
+  expect(page).not_to have_xpath "//script[contains(., icon.geral_u)]"
 end
 
 Then("I should not see a general hospital pin on the map") do
-  expect(page).not_to have_xpath "//script[contains(., icon.geral_u)]", visible: false
+  expect(page).not_to have_xpath "//script[contains(., icon.geral_u)]"
 end
