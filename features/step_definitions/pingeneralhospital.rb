c73041460d51a4f6e05c2ac4c3aa379ad9b60cb1
meugenia1 <meps@cin.ufpe.br>
2017-11-26 21:33:21 -0300

    #83

---
+++ features/step_definitions/pingeneralhospital.rb
@@ -0,0 +1,23 @@
+Given("I am on the mappage") do
+  visit "/map"
+end
+
+And ("there are general hospitals near me") do
+  expect(page).to have_xpath "//script[contains(., icon.pin_hospitalgeral)]", visible: false
+end
+
+Then("I should see a pin on the map on every general hospital location") do
+  expect(page).to have_xpath "//script[contains(., icon.pin_hospitalgeral)]", visible: false
+end
+
+Given("I am on the mappage") do
+  visit "/map"
+end
+
+And ("there are not general hospitals near me") do
+  expect(page).not_to have_xpath "//script[contains(., icon.pin_hospitalgeral)]", visible: false
+end
+
+Then("I should not see a general hospital pin on the map") do
+  expect(page).not_to have_xpath "//script[contains(., icon.pin_hospitalgeral)]", visible: false
+end
